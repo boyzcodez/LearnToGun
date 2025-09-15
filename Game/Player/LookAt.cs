5bfeec28842088535f8fd2bed5d2d406537ea058
Whiteboyz <Korhonen.Tommi@proton.me>
2025-09-15 11:39:41 +0300

    Refactored lookAt marker in player to snap to a certain angle and move back gunspot when looking up or down.

--- Game/Player/LookAt.cs
+++ Game/Player/LookAt.cs
@@ -3,14 +3,42 @@ using System;
 
 public partial class LookAt : Marker2D
 {
+    [Export] public float SnapDegrees = 5f;
+    [Export] private Node2D gunSpot;
+    [Export] private float BaseOffset = 14f;
+    [Export] private float MinOffset = 7f;
     public override void _Process(double delta)
     {
         Vector2 toMouse = GetGlobalMousePosition() - GlobalPosition;
-        Rotation = toMouse.Angle();
+
+        float angle = toMouse.Angle();
+
+        // Convert to degrees
+        float angleDegrees = Mathf.RadToDeg(angle);
+
+        // Snap to nearest increment (e.g., 10°)
+        float snappedDegrees = Mathf.Round(angleDegrees / SnapDegrees) * SnapDegrees;
+
+        // Convert back to radians
+        float snappedAngle = Mathf.DegToRad(snappedDegrees);
+
+
+        Rotation = snappedAngle;
+        //toMouse.Angle();
 
         if (Rotation > -1.5f && Rotation < 1.5f)
             Scale = new Vector2(1, 1);
         else
             Scale = new Vector2(1, -1);
+
+        if (gunSpot != null)
+        {
+            float factor = Mathf.Abs(Mathf.Cos(snappedAngle));
+            float offsetX = Mathf.Lerp(MinOffset, BaseOffset, factor);
+
+            Vector2 spritePos = gunSpot.Position;
+            spritePos.X = offsetX;
+            gunSpot.Position = spritePos;
+        }
     }
 }
