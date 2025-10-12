b4d7a5d1771677e4bae9f66387c15cfb86a10063
boyzcodez <tommi.korh954@gmail.com>
2025-10-12 21:38:39 +0300

    New robot and coin spritesheets. Also made the AnimatedSpriteE work with front and back sprites.

--- Game/Entities/TestEnemies/AnimatedSpriteE.cs
+++ Game/Entities/TestEnemies/AnimatedSpriteE.cs
@@ -3,13 +3,16 @@ using System;
 
 public partial class AnimatedSpriteE : AnimatedSprite
 {
+    private Player player;
     private Entity self;
     private Look look;
+    private string direction = "Front";
 
     public override void _Ready()
     {
         self = GetOwner<Entity>();
         look = GetNode<Look>("Look");
+        player = GetTree().GetFirstNodeInGroup("Player") as Player;
     }
     public override void _Process(double delta)
     {
@@ -18,17 +21,33 @@ public partial class AnimatedSpriteE : AnimatedSprite
         else
             PlayAnimation("Idle", 1);
 
-        if (look.Rotation > -1.5f && look.Rotation < 1.5f)
-            FlipH = false;
+        // if (look.Rotation > -1.5f && look.Rotation < 1.5f)
+        //     FlipH = false;
+        // else
+        //     FlipH = true;
+        Vector2 dir = (player.GlobalPosition - GlobalPosition).Normalized();
+        FlipH = dir.X < 0;
+
+        if (dir.Y > 0)
+        {
+            direction = "Front";
+            look.ShowBehindParent = false;
+        }
         else
-            FlipH = true;
+        {
+            direction = "Back";
+            look.ShowBehindParent = true;
+        }
+        //direction = dir.Y > 0 ? "Front" : "Back";
+
+        
     }
     public override void PlayAnimation(string animation = "", int value = 0)
     {
         if (value >= animationPriority)
         {
             animationPriority = value;
-            Play(animation);
+            Play(animation + direction);
         }
             
     }
