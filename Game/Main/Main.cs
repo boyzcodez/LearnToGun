cee3ecdb39a0a13cd0459e6e295010fd45166eac
boyzcodez <tommi.korh954@gmail.com>
2025-11-21 21:14:31 +0200

    added some code to try and fix fullscreen issue. !! NOT FIXED !! still broken and very unsure as to what is causing it. Maybe Windows being shit.

--- Game/Main/Main.cs
+++ Game/Main/Main.cs
@@ -3,9 +3,19 @@ using System;
 
 public partial class Main : Node2D
 {
-    public override void _Ready()
-    {
-        GD.Print("Hello World!");
-    }
+//     public override void _Ready()
+// {
+//     // DO NOT set fullscreen here; Windows may still be initializing
+//     CallDeferred(nameof(EnableExclusive));
+// }
+
+// private void EnableExclusive()
+// {
+//     // Make sure the game is on the correct monitor
+//     DisplayServer.WindowSetCurrentScreen(0);
+
+//     // Now switch
+//     DisplayServer.WindowSetMode(DisplayServer.WindowMode.ExclusiveFullscreen);
+// }
 
 }
