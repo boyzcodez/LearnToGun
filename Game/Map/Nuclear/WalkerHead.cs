ef373e909f83084e15c4650b1d5f1f59adfa254a
boyzcodez <tommi.korh954@gmail.com>
2025-12-10 16:24:30 +0200

    Very ealry version of the map generator is working.

--- Game/Map/Nuclear/WalkerHead.cs
+++ Game/Map/Nuclear/WalkerHead.cs
@@ -1,15 +1,65 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public partial class WalkerHead : Node2D
 {
-    [Export] public TileMapLayer map;
+    [Export] public int MapLength = 100;
+    [Export] public int PathLength = 100;
+    [Export] public TileMapLayer FloorMap;
+    [Export] public TileMapLayer WallMap;
+
     public override void _Ready()
     {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        FloorMap.Clear();
+        WallMap.Clear();
+
         foreach (WalkerUnit walker in GetChildren())
         {
             walker.CalcPaht();
         }
-        
+
+        BuildWalls();
+    }
+
+    public void BuildWalls()
+    {
+        List<Vector2I> floorSet = new();
+        foreach (WalkerUnit walker in GetChildren())
+        {
+            foreach (var pos in walker.carvedTiles)
+            {
+                if (!floorSet.Contains(pos)) floorSet.Add(pos);
+            }
+        }
+            
+
+        for (int x = -MapLength; x < MapLength; x++)
+        {
+            for (int y = -MapLength; y < MapLength; y++)
+            {
+                var location = new Vector2I(x, y);
+                if (!floorSet.Contains(location)) WallMap.SetCell(location, 0, new Vector2I(1, 0));
+            }
+        }
+    }
+
+    public override void _Input(InputEvent input)
+    {
+        if (input.IsActionPressed("space"))
+        {
+            GenerateMap();
+        }
+    }
+
+
+
+    public void DestroyWall(Vector2I pos)
+    {
+        WallMap.EraseCell(pos);
     }
 }
--- Game/Map/Nuclear/WalkerUnit.cs
+++ Game/Map/Nuclear/WalkerUnit.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 public partial class WalkerUnit : Node2D
 {
     public enum Dirs{LEFT,RIGHT,UP,DOWN}
-    private int PathLength = 100;
-    const int tileSize = 32;
+    private Vector2 floorTile = new Vector2I(0, 0);
+    public List<Vector2I> carvedTiles;
     
 
     public void CalcPaht()
     {
+        carvedTiles = new();
+
+        var parent = GetParent<WalkerHead>();
+        var PathLength = parent.PathLength;
+
         List<int> PathSteps = new();
         for (int i = 0; i < PathLength; i++)
         {
@@ -17,8 +22,8 @@ public partial class WalkerUnit : Node2D
             PathSteps.Add(stepsi);
         }
 
-        Vector2I location = (Vector2I)GetParent<WalkerHead>().GlobalPosition;
-        TileMapLayer tm = GetParent<WalkerHead>().map;
+        Vector2I location = (Vector2I)parent.GlobalPosition;
+        TileMapLayer tm = parent.FloorMap;
 
         foreach (int dir in PathSteps)
         {
@@ -41,6 +46,7 @@ public partial class WalkerUnit : Node2D
             }
             location += ModifierDirection;
             tm.SetCell(location, 0, new Vector2I(0, 0));
+            carvedTiles.Add(location);
         }
 
     }
